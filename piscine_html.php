1dd626b30ee95283484be574e58a2f2672481396
QuentinRP <47528421+QuentinRP@users.noreply.github.com>
2019-05-05 20:58:20 +0200

    Add files via upload

---
+++ piscine_html.php
@@ -0,0 +1,52 @@
+<?php require 'header.php'; ?>
+
+ 		<header class="page-header header container-fluid">
+ 			<div class="overlayp"></div>
+ 			<div class="overlayp"></div>
+			<div class="descriptionp">
+			<style>
+			header {background-image: url('amazonece.jpg');}
+			</style>
+ 				<h1>ECE Amazon</h1>
+ 				<p>
+				Par Prisset Robinson, Ziarczyk-Aumont Paul et Rocha Pinto Quentin.
+ 				</p>
+ 				
+ 	</header>
+
+ 	<div class="container features">
+ 		<div class="row">
+ 			<div class="col-lg-4 col-md-4 col-sm-12">
+ 			<h3 class="feature-titlep" align="center">Offre du moment</h3>
+ 			<img src="iphone-xr.png" class="img-fluid">
+ 			<p align="center">
+ 			Apple Iphone XR 64Go 800€
+ 			</p>
+ 		</div>
+ 		<div class="col-lg-4 col-md-4 col-sm-12">
+			 <h3 class="feature-titlep" align="center">Meilleur prix</h3>
+ 			<img src="airpods2.jpg" class="img-fluid">
+ 			<p align="center">
+ 			Apple Airpods 2 180€
+ 			</p>
+ 		</div>
+	 <div class="col-lg-4 col-md-4 col-sm-12">
+	 	<h3 class="feature-titlep">Entrer en contact!</h3>
+		<div class="form-group">
+ 			<input type="text" class="form-control" placeholder="Votre nom:" name="">
+		</div>
+		<div class="form-group">
+			 <input type="email" class="form-control" placeholder="Courriel:" name="email">
+		</div>
+		<div class="form-group">
+ 			<textarea class="form-control" rows="4">Vos commentaires</textarea>
+		</div>
+		<input type="submit" class="btn btn-secondary btn-block" value="Envoyer" name="">
+ 		</div>
+		</div>
+	</div>
+
+
+
+
+<?php require 'footer.php'; ?>
