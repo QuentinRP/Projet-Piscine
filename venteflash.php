4997dcb3831c3a5aaeae916649987d3f9bca0fc0
QuentinRP <47528421+QuentinRP@users.noreply.github.com>
2019-05-05 20:53:19 +0200

    Add files via upload

---
+++ venteflash.php
@@ -0,0 +1,43 @@
+<?php require 'header.php'; ?>
+
+ 			<header class="page-header header container-fluid">
+ 			<div class="overlay"></div>
+			<div class="description">
+ 				<h1>Toutes nos Ventes Flash</h1>
+			</div>
+
+<div class="container features">
+ 	<div class="row">
+ 		<div class="col-lg-3 col-md-4 col-sm-12">
+ 			<h3 class="feature-titlev" align="center">Meilleur vente <br> Livres:<br></h3>
+ 			<div class="zoom"><a href="tintin.php" ><img src="bd_tintin.jpg" class="img-fluid"/></a></div>
+ 			<h3 class="feature-title2" align="center"><br>TINTIN: On a marché sur la lune </h3>
+ 			<p align="center">- 10€ -</p>
+ 		</div>
+
+ 		<div class="col-lg-3 col-md-4 col-sm-12">
+			 <h3 class="feature-titlev" align="center">Meilleur vente Musiques:<br></h3>
+ 			<div class="zoom"><a href="mj.php" ><img src="mj.jpg" class="img-fluid"/></a></div>
+ 			<h3 class="feature-title2" align="center"><br>Mickeal Jackson Thriller</h3>
+ 			<p align="center">- 10€ -</p>
+ 		</div>
+
+	    <div class="col-lg-3 col-md-4 col-sm-12">
+	 		<h3 class="feature-titlev" align="center">Meilleur vente Vêtements:<br> </h3>
+			<div class="zoom"><a href="levis.php" ><img src="levis.jpg" class="img-fluid"/></a></div>
+			<h3 class="feature-title2" align="center"><br>T-Shirt Levis </h3>
+ 			<p align="center">- 10€ -</p>
+		</div>
+
+		<div class="col-lg-3 col-md-4 col-sm-12">
+	 		<h3 class="feature-titlev" align="center">Meilleur vente <br> Sports et Loisirs:<br> </h3>
+			<div class="zoom"><a href="ballon.php" ><img src="ballon.jpg" class="img-fluid"/></a></div>
+			<h3 class="feature-title2" align="center"><br>Ballon de foot Nike </h3>
+ 			<p align="center">- 10€ -</p>
+		</div>
+	
+	</div>
+</div>
+</header>
+
+<?php require 'footer.php'; ?>
